ac77e242f1d067493a54339906d66ccd8640382b
Cloudyee <gyonggyong98@gmail.com>
2023-12-27 18:00:36 +0900

    add : tsx 모듈 연습

--- starting-project/starting-typeScript/react-and-typescript/src/App.tsx
+++ starting-project/starting-typeScript/react-and-typescript/src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css'
 import Todos from './components/Todos'
+import Todo from './models/todo'
 
 function App() {
+  const todos=[
+    new Todo('리액트 공부하기'),
+    new Todo('타입스크립트 공부하기')
+  ];
+  
   return (
     <>
       <div>
         하이하이요 화이팅
-        <Todos items={['Learn React','Learn TpypeScript']}/>
+        <Todos items={todos}/>
       </div>
     </>
-  )
+  );
 }
 
 export default App
--- starting-project/starting-typeScript/react-and-typescript/src/components/Todos.tsx
+++ starting-project/starting-typeScript/react-and-typescript/src/components/Todos.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+
 //타입을 정의함
 //함수가 함수형 컴포넌트가 됨을 명확히한다.
-const Todos:React.FC<{items:string[]}> =(props)=>{
+const Todos:React.FC<{items:Todo[]}> =(props)=>{
     return(
     <ul>
-        {props.items.map(item=><li key={item}>{item}</li>)}
+        {props.items.map((item) => <TodoItem key={item.id} text={item.text}/>)}
     </ul>
     );
 }
