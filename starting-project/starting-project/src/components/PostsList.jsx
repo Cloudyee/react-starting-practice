2b1ee98f6384cc71ee50fe72778bab165d921dd2
Cloudyee <gyonggyong98@gmail.com>
2023-12-27 13:11:26 +0900

    add: 로딩상태 관리

--- starting-project/starting-project/src/components/PostsList.jsx
+++ starting-project/starting-project/src/components/PostsList.jsx
@@ -1,17 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Post from "./Post";
 import classes from "./PostsList.module.css"
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 
-function PostsList({isPosting, onStopPosting}){
+function PostsList({isPosting, onStopPosting}){ 
+
     const [posts, setPosts]=useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+
+    //두가지 인자로 함수와 배열을 넘겨야한다.
+    //배열에는 의존성을 설정한다.
+    useEffect(()=>{
+        async function fetchPosts(){
+            setIsFetching(true);
+            const response = await fetch('http://localhost:8080/posts')
+            const resData = await response.json()
+            setPosts(resData.posts);
+            setIsFetching(false);
+        }
+
+        fetchPosts();
+    },[]);
 
     function addPostHandelr(postData){
         fetch('http://localhost:8080/posts',{
             method : 'POST',
-            body: JSON.stringify(postData), //stringify(): 데이터가 JSON 포맷으로 바뀜.,
+            body: JSON.stringify(postData), //stringify(): 데이터가 JSON 포맷으로 바뀜
             headers:{
                 'Content-Type' : 'application/json'      //Content-Type에 따라 데이터를 받는 측에서는 데이터를 어떻게 처리해야 할 지 판단
             }
@@ -29,12 +45,15 @@ function PostsList({isPosting, onStopPosting}){
              />
          </Modal>
          )}
-         {posts.length>0&&(
+         {!isFetching&& posts.length>0 &&(
             <ul className={classes.posts}>
                 {posts.map((post)=><Post key={post.body} author={post.author} body={post.body}/>)}
             </ul>
          )}
-         {posts.length === 0 && (<div style={{textAlignLast:'center', color:'white'}}>
+            {isFetching && (<div style={{textAlign : 'center', color : 'white'}}>
+                <p>Loading posts....</p>
+            </div>)}
+         {!isFetching&& posts.length === 0 && (<div style={{textAlignLast:'center', color:'white'}}>
             <h2>There are no posts yet.</h2>
             <p>Please adding some!</p>
             </div>
