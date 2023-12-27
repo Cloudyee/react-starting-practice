0852e248a1d5a999d7d4c40c03ea9c7004138151
Cloudyee <gyonggyong98@gmail.com>
2023-12-27 10:49:30 +0900

    add: 요청 전송과 저장

--- starting-project/starting-project/src/components/PostsList.jsx
+++ starting-project/starting-project/src/components/PostsList.jsx
@@ -9,6 +9,13 @@ function PostsList({isPosting, onStopPosting}){
     const [posts, setPosts]=useState([]);
 
     function addPostHandelr(postData){
+        fetch('http://localhost:8080/posts',{
+            method : 'POST',
+            body: JSON.stringify(postData), //stringify(): 데이터가 JSON 포맷으로 바뀜.,
+            headers:{
+                'Content-Type' : 'application/json'      //Content-Type에 따라 데이터를 받는 측에서는 데이터를 어떻게 처리해야 할 지 판단
+            }
+        }); //브라우저에서 제공, HTTP요청을 보낼 때 사용한다.
         setPosts((exstingPosts)=>[postData, ...exstingPosts]); //기존의 데이터를 유지하며 새로운 포스트를 갱신
     }
 
