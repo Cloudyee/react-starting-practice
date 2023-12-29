2a891b88629921e67f84187a36de628d3ecbb121
Cloudyee <gyonggyong98@gmail.com>
2023-12-29 11:19:21 +0900

    add : redux toolkit 적용 환경 세팅

--- starting-project/redux-project/01-starting-project/src/store/index.js
+++ starting-project/redux-project/01-starting-project/src/store/index.js
@@ -1,6 +1,16 @@
 import {createStore} from 'redux'
-const intialState = {counter:0, showCounter:true};
 
+//식별자의 이름을 한번에 정의하고 이를 활용할 수 있다.
+//프로젝트가 커져 다양한 상태가 생기게 되면, 이러한 상태들을 한번에 복사해야하는 상황이 올 수 있다.
+//이러한 상황을 해결하기 위해 
+/*
+1. 아래와같이 설정하여 활용할 수 있다.
+2. 리듀서를 어려개의 리듀서로 나눌 수 있다.
+3. Redux toolkit이라는 써드 파티를 활용한다! => 해당 방법을 채택하여 활용한다!!!
+*/
+export const INCREMENT = 'increment';
+
+const intialState = {counter:0, showCounter:true};
 const counterReducer = (state=intialState, action)=>{
     if(action.type==='increment'){
         return {
