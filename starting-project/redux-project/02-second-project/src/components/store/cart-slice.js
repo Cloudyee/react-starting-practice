f4e3ad7612d039331a0d9b4b519ab447218371ad
Cloudyee <gyonggyong98@gmail.com>
2024-01-02 13:59:58 +0900

    error : 장바구니 리스트 표출 중 에러 발생

--- starting-project/redux-project/02-second-project/src/components/store/cart-slice.js
+++ starting-project/redux-project/02-second-project/src/components/store/cart-slice.js
@@ -11,10 +11,17 @@ const cartSlice = createSlice({
             const newItem = action.payload;
             //해당 항목이 이미 존재하는지 여부 확인
             const existingItem = state.items.find(item=> item.id === newItem.id);
+            state.totalQuantity++;
             if(!existingItem){
                 //리덕스는 원래 본래의 값을 바꾸면 안된다.
                 //리덕스 툴킷에서 이를 자동으로 설정해주기 때문에 우리는 .push를 활용할 수 있다.
-                state.items.push({itemId: newItem.id, price: newItem.price, quantity: 1, totalPrice : newItem.price, name: newItem.title});
+                state.items.push({
+                    id: newItem.id, 
+                    price: newItem.price, 
+                    quantity: newItem.quantity, 
+                    totalPrice : newItem.price, 
+                    name: newItem.title
+                });
             }else{
                 existingItem.quantity++;
                 existingItem.totalPrice = existingItem.totalPrice+newItem.price;
@@ -23,6 +30,7 @@ const cartSlice = createSlice({
         removeItemFromCart(state, action){
             const id = action.payload;
             const existingItem = state.items.find(item=>item.id ===id);
+            state.totalQuantity--;
             if(existingItem.quantity === 1){
                 //id와 일치하지 않는 모든 항목을 유지
                 state.items = state.items.filter(item=>item.id!==id);
