06ec4dc5a33a721d577ceba216d43a4c3daf1ad8
Cloudyee <gyonggyong98@gmail.com>
2024-01-08 14:07:47 +0900

    refactor : 코드 리팩터링

--- starting-project/tic-tac-toe-starting-project/src/App.jsx
+++ starting-project/tic-tac-toe-starting-project/src/App.jsx
@@ -5,6 +5,17 @@ import GameOver from "./components/GameOver";
 import { useState } from "react"
 import { WINNING_COMBINATIONS } from "./components/winning-combination";
 
+const PLAYERS={
+  X:'Player 1',
+  O:'Plyaer 2'
+}
+
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
+
 function deriveActivePlayer(gameTurns){
   let currentPlayer='X';
       if(gameTurns.length>0 && gameTurns[0].player==='X'){
@@ -13,17 +24,42 @@ function deriveActivePlayer(gameTurns){
   return currentPlayer;
 }
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-]
+//승자를 가리는 로직을 메서드로 독립
+function deriveWinner(gameBoard, players){
+  let winner;
+  for(const combination of WINNING_COMBINATIONS){
+    //단계적으로 검토하며 기호를 확인
+    //제시된 기호들은 승리하는 경우의 수를 모두 포함함으로
+    //아래의 세 단계를 거쳐 승자를 추릴 수 있는 것이다!
+    const firstSquareSymbol= gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol=gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol=gameBoard[combination[2].row][combination[2].column];
+
+    //우승 조합의 세가지 버튼이 모두 같은 부호
+    if(firstSquareSymbol && firstSquareSymbol===secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){ 
+      winner=players[firstSquareSymbol];
+
+      return winner;
+    }
+  }
+}
+
+function deriveGameBoard(gameTurns){
+    //기존 배열에 새로운 배열을 덮어 씌위도록 설정
+    let gameBoard = [...INITIAL_GAME_BOARD.map(array=> [...array])];
+
+    for(const turn of gameTurns){
+        const{square, player} = turn;
+        const {row, col} = square;
+  
+        gameBoard[row][col] = player;
+    }
+
+    return gameBoard;
+}
 
 function App() {
-  const [players , setPlayers]= useState({
-    'X':'Player 1',
-    'O' : 'Player 2'
-  });
+  const [players , setPlayers]= useState(PLAYERS);
   //기존 코드에서 상태 관리를 하나로 리팩터링 해주었다.
   //상태변수의 초기값은 빈 배열이다. => GameBoard에서 버튼을 disabled하는 조건을 여기서 확인 가능하다.
   const[gameTurns, setGameTurns] = useState([]);
@@ -32,41 +68,13 @@ function App() {
   // const [hasWinner , setHasWinner] = useState();
   
   const activePlayer = deriveActivePlayer(gameTurns);
-  
-  //기존 배열에 새로운 배열을 덮어 씌위도록 설정
-  let gameBoard = [...initialGameBoard.map(array=> [...array])];
+  const gameBoard = deriveGameBoard(gameTurns);
 
-  for(const turn of gameTurns){
-      const{square, player} = turn;
-      const {row, col} = square;
-
-      gameBoard[row][col] = player;
-  }
-
-    let winner;
-
-    for(const combination of WINNING_COMBINATIONS){
-      //단계적으로 검토하며 기호를 확인
-      //제시된 기호들은 승리하는 경우의 수를 모두 포함함으로
-      //아래의 세 단계를 거쳐 승자를 추릴 수 있는 것이다!
-      const firstSquareSymbol= gameBoard[combination[0].row][combination[0].column];
-      const secondSquareSymbol=gameBoard[combination[1].row][combination[1].column];
-      const thirdSquareSymbol=gameBoard[combination[2].row][combination[2].column];
-
-      //우승 조합의 세가지 버튼이 모두 같은 부호
-      if(firstSquareSymbol && firstSquareSymbol===secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
-        
-        winner=players[firstSquareSymbol];
-      }
-    }
-
-    const hasDraw = gameTurns.length === 9 && !winner;
+  const winner = deriveWinner(gameBoard, players);
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex){
-    // setActivePlayer((curActivePlayer)=>curActivePlayer==='X'? 'O' : 'X');
     setGameTurns(prevTurns => {
-      // const currentPlayer = deriveActivePlayer(prevTurns);
-
       const updatedTurns = [{square : {row:rowIndex, col:colIndex}, player:activePlayer},
         ...prevTurns];
 
@@ -91,8 +99,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="player 1" symbol='X' isActive={activePlayer==='X'} onChangeName={handlePlayerNameChange}/>
-          <Player initialName="player 2" symbol='O' isActive={activePlayer==='O'} onChangeName={handlePlayerNameChange}/>
+          <Player initialName={PLAYERS.X} symbol='X' isActive={activePlayer==='X'} onChangeName={handlePlayerNameChange}/>
+          <Player initialName={PLAYERS.O} symbol='O' isActive={activePlayer==='O'} onChangeName={handlePlayerNameChange}/>
         </ol>
         {(winner || hasDraw) && <GameOver winner = {winner} onRestart={handleRestart}/>}
         <GameBoard 
